7512ce3751f7c74e0904365ae3013548062bf373
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-02-15 14:01:31 -0500

    Added transform on nav as and h1.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
 // Your code goes here
-const button = document.querySelectorAll(".btn");
-
-button.forEach(but => {
-  but.addEventListener("dblclick", () => {
-    but.style.backgroundColor = "red";
-  });
-});
-
+// Enlarge the photos
 const imgsel = document.querySelectorAll("img");
 imgsel.forEach(i => {
   i.addEventListener("dblclick", () => {
     i.classList.toggle("large");
+  });
+});
+
+const title1 = document.querySelector("h1");
+title1.addEventListener("click", () => {
+  title1.style.color = "red";
+  title1.style.transform = "scale(1.5)";
+});
+
+const nava = document.querySelectorAll("a");
+nava.forEach(h => {
+  h.addEventListener("mouseenter", () => {
+    h.style.transform = "scale(1.2)";
   });
 });
