855ff215d8cef4ddc18751817f080798e253432d
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-02-14 20:19:41 -0500

    Figured out the double clicking the pictures.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const button = document.querySelectorAll(".btn");
+
+button.forEach(but => {
+  but.addEventListener("dblclick", () => {
+    but.style.backgroundColor = "red";
+  });
+});
+
+const imgsel = document.querySelectorAll("img");
+imgsel.forEach(i => {
+  i.addEventListener("dblclick", () => {
+    i.classList.toggle("large");
+  });
+});
