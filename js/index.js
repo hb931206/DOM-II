d599cc3f5220d0ea95dad66301901b5f02dd99fd
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-02-18 07:27:31 -0500

    Finished the nav prevent default and 1/2 done with stop propagation.

--- js/index.js
+++ js/index.js
@@ -7,6 +7,11 @@ imgsel.forEach(i => {
   });
 });
 
+const imgConSel = document.querySelector(".img-content");
+imgConSel.addEventListener("click", () => {
+  event.stopPropagation();
+});
+
 const title1 = document.querySelector("h1");
 title1.addEventListener("mouseenter", () => {
   title1.style.color = "red";
@@ -22,6 +27,12 @@ const nava = document.querySelectorAll("a");
 nava.forEach(h => {
   h.addEventListener("mouseover", () => {
     h.style.transform = "scale(1.3)";
+  });
+});
+
+nava.forEach(e => {
+  e.addEventListener("click", () => {
+    e.preventDefault();
   });
 });
 
@@ -64,13 +75,16 @@ function zoom(event) {
 
   scale += event.deltaY * -0.01;
 
-  // Restrict scale
   scale = Math.min(Math.max(0.125, scale), 4);
 
-  // Apply scale transform
   el.style.transform = `scale(${scale})`;
 }
 
 let scale = 1;
 const el = document.querySelector("h1");
 el.addEventListener("wheel", zoom);
+
+const mainContent = document.querySelector(".content-section");
+mainContent.addEventListener("click", () => {
+  mainContent.style.background = "red";
+});
